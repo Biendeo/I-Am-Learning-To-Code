6d471943c0344250ed73c56c9a3506caf6d547a5
Biendeo <pantherman1996@hotmail.com>
2015-03-05 08:36:13 +1100

    Fixed a couple of issues with the formatting of the program. I also made it reset the calculation values when you do a second operation. There's still a little bit of an issue with typing wrong bits in the getchar sections, but at least the program works completely properly.

--- 001 - C Simple Counter/SimpleCounter.c
+++ 001 - C Simple Counter/SimpleCounter.c
@@ -25,15 +25,15 @@ int main() {
 	// This is here just to restart the application when asked to at the end.
 	beginning:
 	// Firstly, I'm asking what number they're reaching to, and storing that as integer z.
-	printf("Okay, what number do you want to reach?");
+	printf("\nOkay, what number do you want to reach? ");
 	scanf("%d", &z);
 
 	// Then, I'm asking how much they want to go up by, and storing that in y.
-	printf("What increment do you want to add by to get there?");
+	printf("\nWhat increment do you want to add by to get there? ");
 	scanf("%d", &y);
 
 	// Then I'm giving them a chance to re-read this, and see if anything they'd like to correct.
-	printf("So you want to go up by %d to hit %d? (Y/N)", y, z);
+	printf("\nSo you want to go up by %d to hit %d? (Y/N) ", y, z);
 	// Here, there's a label, and q is reset. This is because the other conditions later link back to here.
 	question: 
 	// This gets the letter that they type, and then it's passed through some logic.
@@ -49,25 +49,34 @@ int main() {
 	// If it's a no, then the earlier questions are asked again (with different text), and it loops.
 	else if (q == 'n' || q == 'N')
 	{
-		printf("In that case, what number did you want to count to?");
+		printf("\nIn that case, what number did you want to count to? ");
 		scanf("%d", &z);
-		printf("And what increment do you want to add by to get there?");
+		printf("\nAnd what increment do you want to add by to get there? ");
 		scanf("%d", &y);
+		printf("\nSo you want to go up by %d to hit %d? (Y/N) ", y, z);
 		q = "0";
 		goto question;
+	}
+	// If the question hasn't been answered, don't say a different answer has popped up.
+	else if (q = "0")
+	{	
+	goto question;
 	}
 
 	// If it's neither, the question is asked again.
 	else
 	{
 		q = "0";
-		printf("That wasn't what I was expecting, please type a Y or an N.");
+		printf("\nThat wasn't what I was expecting, please type a Y or an N. ");
 		goto question;
 	}
 
 	// Now we're up to the counting part.
 	counting:
-
+	// I'm going to reset the numbers in case you're doing a second operation.
+	x = 0;
+	w = 0;
+	
 	// So x is the current number we're up to. While it's less than z, we do the operation.
 	while (x < z)
 	{
@@ -76,7 +85,7 @@ int main() {
 		// We also add one to the number of operations we've done.
 		w++;
 		// And print this to the user.
-		printf("I've added %d times, and now I'm up to %d.", w, x);
+		printf("\nI've added %d times, and now I'm up to %d.", w, x);
 	}
 
 	// Now we've overshot it, so we just back up a step.
@@ -84,10 +93,10 @@ int main() {
 	w--;
 	// And we tell the person of our results.
 	v = z - x;
-	printf("Okay, I added %d %d times, and got up to %d. That's %d short of %d.", y, w, x, v, z);
+	printf("\nOkay, I added %d %d times, and got up to %d. That's %d short of %d.", y, w, x, v, z);
 
 	// We finally end with a similar yes/no to what we had before, only now they do different stuff.
-	printf("Want to do another one? (Y/N) ");
+	printf("\nWant to do another one? (Y/N) ");
 
 	ending:
 	q = 0;
@@ -100,12 +109,13 @@ int main() {
 
 	else if (q == 'n' || q == 'N')
 	{
-		printf("Thanks for using this. See ya! /n");
+		printf("\nThanks for using this. See ya!\n");
 	}
 
 	else
 	{
-		printf("I don't know what you meant by that. Type Y or N please. ");
+		printf("\nI don't know what you meant by that. Type Y or N please. ");
+		q = 0;
 		goto ending;
 	}
 }
