e9ce3dea7d24ccd4b94d4ab9c862d753da52f589
Biendeo <pantherman1996@hotmail.com>
2015-03-04 17:00:57 +1100

    Added a second program here. It's a simple "Hello World" like program basically. It's commented, so that's good.

---
+++ 002 - HELLO BANANAS/HELLO.c
@@ -0,0 +1,12 @@
+// Start by including this classic header.
+#include <stdio.h>
+
+// This is the main function, it starts normally.
+int main () {
+	
+	// This prints stuff on the screen with a new line on the end.
+	printf("Bananas!\n");
+	
+	// This tells the computer 0. In case.
+	return 0;
+}
