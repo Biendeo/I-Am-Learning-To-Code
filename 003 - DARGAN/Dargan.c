324f56a0cae3af4769289cda5ad27d22d1e81691
Biendeo <pantherman1996@hotmail.com>
2015-03-06 11:38:10 +1100

    A first push of Dargan.c: a program that prints a lot of ASCII stuff. It's a little messy, but it basically prints out Trogdor. There will be a more legitimate version later (with my own terrible ASCII graphics).

---
+++ 003 - DARGAN/Dargan.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+// Every line starts with the \n because it needs to print on a new line each time.
+// Also, every ', ", and \ needs an additional \ in front of it, or else it thinks it's a term.
+// I also shamelessly rip the ASCII from this page: http://www.retrojunkie.com/asciiart/cartchar/homestar.htm
+int main () {
+	printf("\n                                                 :::");
+	printf("\n                                             :: :::.");
+	printf("\n                       \\/,                    .:::::");
+	printf("\n           \\),          \\`-._                 :::888");
+	printf("\n           /\\            \\   `-.             ::88888");
+	printf("\n          /  \\            | .(                ::88");
+	printf("\n         /,.  \\           ; ( `              .:8888");
+	printf("\n            ), \\         / ;``               :::888");
+	printf("\n           /_   \\     __/_(_                  :88");
+	printf("\n             `. ,`..-\'      `-._    \\  /      :8");
+	printf("\n               )__ `.           `._ .\\/.");
+	printf("\n              /   `. `             `-._______m         _,");
+	printf("\n  ,-=====-.-;\'                 ,  ___________/ _,-_,\'\"`/__,-.");
+	printf("\n C   =--   ;                   `.`._    V V V       -=-\'\"#==-._");
+	printf("\n:,  \\     ,|      UuUu _,......__   `-.__A_A_ -. ._ ,--._ \",`` `-");
+	printf("\n||  |`---\' :    uUuUu,\'          `\'--...____/   `\" `\".   `");
+	printf("\n|`  :       \\   UuUu:");
+	printf("\n:  /         \\   UuUu`-._");
+	printf("\n \\(_          `._  uUuUu `-.");
+	printf("\n (_3             `._  uUu   `._");
+	printf("\n                    ``-._      `.");
+	printf("\n                         `-._    `.");
+	printf("\n                             `.    \\");
+	printf("\n                               )   ;");
+	printf("\n                              /   /");
+	printf("\n               `.        |\\ ,\'   /");
+	printf("\n                 \",_A_/\\-| `   ,\'");
+	printf("\n                   `--..,_|_,-\'\\");
+	printf("\n                          |     \\");
+	printf("\n                          |      \\__");
+	printf("\n                          |__");
+	return 0;
+}
