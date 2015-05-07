e2b7b5838686322b02f4ffd9f5a298756b53f354
Biendeo <pantherman1996@hotmail.com>
2015-05-07 12:40:24 +1000

    A small push, the program now uses an icon. :)

--- 0!! - Terminal Wars/Compile And Test Program.bat
+++ 0!! - Terminal Wars/Compile And Test Program.bat
@@ -1,5 +1,5 @@
 cd "%~dp0End Files"
-gcc GameUI.c MainGame.c DrawingUI.c FileIO.c GameAI.c -o TerminalWarsTEST.exe -Wall -lws2_32
+gcc GameUI.c MainGame.c DrawingUI.c FileIO.c GameAI.c icon.res -o TerminalWarsTEST.exe -Wall -lws2_32
 pause
 cd ..
 move "%~dp0End Files\TerminalWarsTest.exe" "%~dp0"
