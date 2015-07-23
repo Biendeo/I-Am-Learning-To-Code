0ae58d5d4bb32a0971c5c26d3ba19d7c4c97682b
Biendeo <pantherman1996@gmail.com>
2015-07-24 07:56:05 +1000

    Starting work on a calculator program. Some rough plan notes. I won't start programming until I come up with a logical way to do this.

---
+++ 012 - Calculator/Calculator.cpp
@@ -0,0 +1,23 @@
+/*
+ * A C++ calculator by Thomas Moffet
+ * Started on 23/07/2015
+ * 
+ * The goal of this is to:
+ * 1. Make a working calculator that computes several basic functions.
+ * 2. Experiment using C++ classes in a proper context.
+ * 3. Look good for uni points. :)
+ * 
+ * There is more info in the Plan.txt file provided.
+ *
+ * You can freely use this for whatever you want. :)
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+int main(int argc, char *argv[]) {
+
+    return EXIT_SUCCESS;
+}
