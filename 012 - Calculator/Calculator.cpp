d6e087c652b0caa12f75da3176cc961887c01a8b
Biendeo <pantherman1996@gmail.com>
2015-07-24 18:46:06 +1000

    Added a bunch of stuff to do with the memory stuff. It now has a default constructor, and can set variables at will. It also can get properties from it. The main memory can add variables, and get the name of one given its index, or vice versa. Also, the main now creates and destroys the calculator to test that it loads properly.

--- 012 - Calculator/Calculator.cpp
+++ 012 - Calculator/Calculator.cpp
@@ -38,48 +38,162 @@ class calc {
     // different categories of mathematical terms that can be used in the
     // program.
     enum objectType {
-        null,
-        number,
-        operatortype,
-        constant,
-        variable
+        typeNull,
+        typeNumber,
+        typeOperator,
+        typeConstant,
+        typeVariable,
+        typeEnd
     };
 
-    // This defines a constant as a usable name in the program, and a value
-    // associated with it. It has a boolean to determine whether it is a
-    // constant or not, which allows it to be modified.
-    class variable {
-        bool isConstant;
 
-        public:
-        char *name;
-        double value;
-    };
 
+    // This defines the section that stores variables and the history.
     class memory {
 
+        // This defines a constant as a usable name in the program, and a value
+        // associated with it. It has a boolean to determine whether it is a
+        // constant or not, which allows it to be modified.
+        class variable {
+            bool isConstant;
+            char *name;
+            double value;
+
+            public:
+            // This is the default constructor. It doesn't have any particular
+            // properties to use. Since all the variables are created at launch,
+            // there is no constructor explicitly for giving properties.
+            variable() {
+                isConstant = true;
+                name = "null";
+                value = 0;
+            };
+
+            // The properties are determined after the variable is created, so this
+            // function allows the program to set those.
+            int setVariable(char *givenName, double givenValue, bool givenConstant) {
+                isConstant = givenConstant;
+                name = givenName;
+                value = givenValue;
+            };
+
+            // This function returns whether the variable is a constant;
+            bool getIsConstant() {
+                return isConstant;
+            };
+
+            // This function returns the variable's name.
+            char *getName() {
+                return name;
+            };
+
+            // This function returns the variable's value.
+            double getValue() {
+                return value;
+            }
+        };
+
+        // TODO: Should this be inside the memory class, or should it be outside the whole thing?
+        class history {
+
+        };
+
+
+        // This is the maximum number of variables that the program can store.
+        // This includes constants, so be sure to increase/decrease this
+        // number accordingly. You don't want too many constants to fill up.
+        static const unsigned int MAX_VARIABLES = 250;
+
+        variable variables[MAX_VARIABLES];
+        int totalVariables;
+
+        public:
+        memory() {
+            totalVariables = 0;
+            // When creating constants, call add variable for them.
+        };
+
+        // This function adds a variable to the stored array. It returns an
+        // error if it's full, or if there's another variable by that name.
+        int addVar(char *name, double value, bool isConstant) {
+            // TODO: Replace this with a switch/case for the name.
+            if (name == "null") {
+                return -3; // TODO: Enum this error code.
+            } else if (name == "var") {
+                return -2; // TODO: Enum this error code.
+            } else if (totalVariables == MAX_VARIABLES) {
+                return -1; // TODO: Enum this error code.
+            } else {
+                int i = 0;
+                // I feel as if something can go wrong here. If i reaches
+                // MAX_VARIABLES, then there's no fall protection. However, I
+                // feel as if the previous else if solves that. It seems odd.
+                while ((i < MAX_VARIABLES) && (getVarName(i) == "null")) {
+                    i++;
+                }
+
+                variables[i].setVariable(name, value, isConstant);
+                totalVariables++;
+                return 0; // TODO: Enum this error code.
+            }
+        };
+
+        // This function returns the name of a stored variable given the ID. It
+        // returns "null" if there's one not stored there.
+        char *getVarName(unsigned int index) {
+            if (index >= MAX_VARIABLES) {
+                return "null";
+            } else {
+                return variables[index].getName;
+            }
+        };
+
+        unsigned int getVarIndex(char *name) {
+            int i = 0;
+            while ((i < MAX_VARIABLES) && (getVarName(i) != name)) {
+                i++;
+            }
+
+            if (i == MAX_VARIABLES) {
+                return -1; // TODO: Enum this error code.
+            } else {
+                return i;
+            }
+        }
     };
 
-    class history {
-
-    };
-
+    // TODO: The equation class.
     class equation {
+        char *originalEquation;
 
     };
 
     // This defines a mathematical object as a type and a value of some kind.
-    // type == number,       value == double
-    // type == operatortype, value == operatorType
-    // type == constant,     value == variable
-    // type == variable,     value == variable
+    // type == typeNumber,   value == double
+    // type == typeOperator, value == operatorType
+    // type == typeConstant, value == variable
+    // type == typeVariable, value == variable
+    // type == typeEnd,      value == NULL
     class object {
         objectType type;
         void *value;          // This void * means that it's not type dependent.
+        object *next;
+    };
+
+    public:
+    calc() {
+        memory mem;
     };
 };
 
 int main(int argc, char *argv[]) {
+
+    calc *calculator = new calc;
+
+    // TODO: The program should run some tests before it can be used.
+    // TODO: Also make a program.
+
+    delete calculator;
 
     return EXIT_SUCCESS;
 }
